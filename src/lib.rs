358e2c84fbe88e14d8ec4ecfea033e49d929f6c3
brofrain <knwelc@gmail.com>
2023-08-08 18:56:15 +0200

    docs: update macro description

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-/// Transforms Variant Groups in the given string literals.
+/// Builds a single [String] from provided string-like arguments and transforms Variant Groups.
 ///
 /// # Example
 ///
@@ -7,12 +7,29 @@
 ///
 /// assert_eq!(uno!["text-red"], "text-red");
 ///
-/// assert_eq!(uno!["text-(red sm)"], "text-red text-sm");
-///
 /// assert_eq!(
 ///     uno!["text-(blue lg)", "placeholder:(italic text-(red sm))"],
 ///     "text-blue text-lg placeholder:italic placeholder:text-red placeholder:text-sm"
 /// );
+///
+/// let some = Some("text-red");
+/// let none = None::<String>;
+/// let truthy = true;
+/// let falsy = false;
+/// assert_eq!(uno![
+///         some,
+///         none,
+///         "text-green" => truthy,
+///         "text-black" => falsy,
+///         "",
+///         Some("text-white").map(|_| "text-blue"),
+///         None::<String>,
+///     ],
+///     "text-red text-green text-blue"
+/// );
+///
+/// let truthy = true;
+/// assert_eq!(uno!["text-(sm center)" => truthy], "text-sm text-center");
 /// ```
 #[macro_export]
 macro_rules! uno {
